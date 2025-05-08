6bf970be931035207fcc423be023a1d1718fb3ba
Craig Hopgood <searaig@gmail.com>
2025-05-08 08:08:44 -0400

    add ForgeConfig,OceanConfig types

--- model.py
+++ model.py
@@ -7,3 +7,17 @@ class WebappConfig:
     hostname: str
     username: str
     password: str
+
+
+@dataclass
+class ForgeConfig:
+    apiurl: str
+    apikey: str
+    server_id: int
+    site_id: int
+
+
+@dataclass
+class OceanConfig:
+    apiurl: str
+    apikey: str
